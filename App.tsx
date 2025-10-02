80f3bd1aa2efcfb82dcceb660385d338328d9c5f
Muhamad Akbar Rizky S <akbarrizky0909@gmail.com>
2025-10-02 13:41:07 +0700

    update create new display - tugas sesi 2 android

--- App.tsx
+++ App.tsx
@@ -1,45 +1,70 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import { SafeAreaView, View, Text, TouchableOpacity, ImageBackground, StyleSheet } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
+const colors = {
+  white: '#FFFFFF',
+  gray: '#A9A9A9',
+  button: '#00B8B0',
+  black: '#000000',
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  contentWrapper: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    padding: 20,
+  },
+  titleText: {
+    fontSize: 36,
+    color: colors.white,
+    marginBottom: 10,
+    fontWeight: '600',
+  },
+  subTitle: {
+    fontSize: 14,
+    color: colors.gray,
+    marginBottom: 20,
+    fontWeight: '400',
+  },
+  button: {
+    backgroundColor: colors.button,
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 36,
+    alignSelf: 'flex-start',
+  },
+  buttonText: {
+    color: colors.black,
+    fontSize: 16,
+    fontWeight: '700',
+  },
 });
 
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('./src/assets/bg-android.png')}
+        style={{ flex: 1 }}
+      >
+        <View style={styles.contentWrapper}>
+          <Text style={styles.titleText}>Your Next Adventure Starts Here</Text>
+          <Text style={styles.subTitle}>
+            Life’s too short to stay in one place. Find your next favorite city, beach, or mountain and let's get moving
+          </Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>
+              Start Exploring
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
 export default App;
+
